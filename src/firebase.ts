b0cd27fd4b55155f3bf8d044ff559fb99ed2f13a
ambertaal <amber.taal@soprasteria.com>
2025-10-29 15:35:43 +0100

    feat: revert firebase config via local env

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  apiKey: 'AIzaSyAccZotMLEP5BgVSRj6QS-9Imj2U5ErPng',
+  authDomain: 'scrumpoker-vue.firebaseapp.com',
+  projectId: 'scrumpoker-vue',
+  storageBucket: 'scrumpoker-vue.firebasestorage.app',
+  messagingSenderId: '616336610177',
+  appId: '1:616336610177:web:da1f3e19448548d9234e80',
+  databaseURL: 'https://scrumpoker-vue-default-rtdb.europe-west1.firebasedatabase.app',
 };
 
 const app = initializeApp(firebaseConfig);
